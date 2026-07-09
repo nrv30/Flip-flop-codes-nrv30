31034cca351fce92205d5b44b7e76961d9d79d23
nrv <nirvairverma@gmail.com>
2026-07-08 17:42:25 -0700

    Finished Puzzle03

---
+++ 2026/Puzzle03/main.ps1
@@ -0,0 +1,67 @@
+function Rules ($content, $char) { 
+    $strongest_password = ''
+    $sum = 0
+    $content | ForEach-Object {
+        $password = $_ + $char
+        $score = 0
+        if ($password -cmatch '[a-z]') { 
+            $score++
+        }
+        
+        if ($password -cmatch '[A-Z]') {
+            $score++
+        }
+                
+        $flag = $true
+        $matches = [regex]::Matches($password, '[0-9]')
+        if ($matches.Count -gt 0) {
+            $score++
+            foreach ($match in $matches) {
+                if ($match -match '[012345689]') {
+                    $flag = $false
+                    break
+                }
+            }
+            
+            if ($flag) {
+                $score += 7
+            }
+        }
+        
+        $matches = [regex]::Matches($password, '(.)\1{2,}')
+        if ($matches.Count -gt 0) {
+            $longest = $matches | 
+                           Sort-Object Length -Descending | 
+                           Select-Object -First 1
+            $score += $longest.length * $longest.length
+        } 
+        
+        if ($password -cmatch 'red|blue|green') {
+            $score *= 3
+        }
+        
+        $strength = $score * $password.length
+        if ($strength -gt $max) {
+                $max = $strength
+                $strongest_password = $password
+        }
+        
+
+        $sum += $strength
+    }
+    
+    return $sum
+}
+
+$content = Get-Content $args[0]
+$chars = @('7', 'd', 'n', 'e')
+$max = 0
+
+foreach ($char in $chars) {
+    $sum = Rules $content $char
+    if ($sum -gt $max) {
+        $max = $sum
+    }
+}
+
+write-host $max
